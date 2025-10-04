eb210163c82b6f266b676b882c4c4b4dad393f0a
Marlon2132 <troffstas752@gmail.com>
2025-10-04 20:22:06 +0300

    The dance stage was created.

--- src/main/java/com/github/marlon2132/dancearrangementhelper/DAH_Application.java
+++ src/main/java/com/github/marlon2132/dancearrangementhelper/DAH_Application.java
@@ -3,17 +3,48 @@ package com.github.marlon2132.dancearrangementhelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class DAH_Application extends Application {
+    private Stage main_stage_;
+    private BorderPane main_window_layout_;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DAH_Application.class.getResource("main-window.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage main_stage) throws IOException {
+        this.main_stage_ = main_stage;
+        this.main_stage_.setTitle("DAH");
+
+        initMainWindowLayout();
+        initDanceStageLayout();
+    }
+
+    public void initMainWindowLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(DAH_Application.class.getResource("main-window.fxml"));
+            main_window_layout_ = (BorderPane) loader.load();
+            Scene scene = new Scene(main_window_layout_);
+            main_stage_.setScene(scene);
+            main_stage_.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initDanceStageLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(DAH_Application.class.getResource("dance-stage.fxml"));
+            AnchorPane dance_stage = (AnchorPane) loader.load();
+            main_window_layout_.setCenter(dance_stage);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
